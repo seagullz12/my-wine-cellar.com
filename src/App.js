0d1cac8e08d633142ba11c4a79b2fb2a3f7f1e76
seagullz12 <stefanhoogers@gmail.com>
2024-09-16 17:37:56 +0200

    Deploy with 404 fix

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import WineList from './components/WineList';
 import WineRecommendations from './components/WineRecommendations';
 import NavBar from './components/NavBar'; // Import the NavBar component
+import NotFound from './components/NotFound';
 
 const App = () => {
   return (
@@ -15,7 +16,7 @@ const App = () => {
           <Route path="/wine-scanner" element={<Home />} />
           <Route path="/wine-scanner/cellar" element={<WineList />} />
           <Route path="/wine-scanner/personal-sommelier" element={<WineRecommendations />} />
-          {/* <Route path="*" element={<NotFound />} />  */}
+          <Route path="*" element={<NotFound />} /> 
           {/* Catch-all route */}
         </Routes>
       </div>
---
+++ src/components/NotFound.js
@@ -0,0 +1,29 @@
+// src/components/NotFound.js
+
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+//import './NotFound.css'; // Optional: if you have specific styles
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Redirect to home page after 5 seconds
+    const timer = setTimeout(() => {
+      navigate('/wine-scanner');
+    }, 10);
+
+    // Cleanup timer if the component is unmounted before the timer finishes
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      <p>Redirecting to home page in 5 seconds...</p>
+    </div>
+  );
+};
+
+export default NotFound;
